13dba4edf3bed87e4f8e79c53c7c84f66d87c048
Alessandra Espinoza Ramos <aless.02996@hotmail.com>
2023-07-26 00:06:42 -0500

    main

---
+++ variable2.js
@@ -0,0 +1,20 @@
+// operadores de multiplicaciones variables
+
+//operador Sumas
+
+console.log(3 + 5);
+
+
+// operador restar 
+
+console.log(14-7);
+
+
+// operador multiplicar
+console.log(7*36);
+
+
+// operador division
+
+console.log(36*2);
+
