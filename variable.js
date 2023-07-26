15d2e4c4846b7b09b67218ef620c8e8acc498177
Alessandra Espinoza Ramos <aless.02996@hotmail.com>
2023-07-25 22:34:02 -0500

    cambios

---
+++ variable.js
@@ -0,0 +1,27 @@
+//string 
+
+var bootcamp ='Alessandra';
+
+//Numero
+
+var numeroEntero = 17;
+
+var numeronegativo = -4;
+
+var numeroDecimales = 11.75;
+
+//Booleanos
+
+var estoyEntendido = true;
+
+var estoyaburrido = false;
+
+//Undefined
+
+var cajon1;
+
+console.log(cajon1);
+
+//null
+
+var sinNada = null;
