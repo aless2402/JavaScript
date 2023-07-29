bb25ea4d38250ed4648acc5e9cb69e5f12c19a5c
Alessandra Espinoza Ramos <aless.02996@hotmail.com>
2023-07-29 00:29:31 -0500

    objetivo math operacion

---
+++ objetivo_math.js
@@ -0,0 +1,35 @@
+//math pow 
+Math.pow(2,3);
+//8
+
+
+// Math.round & Math.floor & Math.ceil
+
+Math.round(0.49);
+//0
+
+Math.round(0.50);
+//1
+
+Math.floor(5.93);
+//3
+
+Math.ceil(3.27);
+//4
+
+
+// Math.max & Math.min
+
+Math.max(1,2,3,4,5);
+//5
+
+Math.min(1,2,3,4,5);
+//1
+
+
+// Math.random
+
+Math.random();
+//0.8051478163546508
+
+
