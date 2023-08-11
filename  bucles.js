929ba284c0943c80d2d732a96593e17971eec3e6
Alessandra Espinoza Ramos <aless.02996@hotmail.com>
2023-08-10 23:53:02 -0500

    bucle in to FOR OR WHILE

---
+++  bucles.js
@@ -0,0 +1,22 @@
+var suma = 0 ;
+suma = suma +1 ; 
+suma = suma +2 ; 
+suma = suma +3 ; 
+suma = suma +4 ; 
+suma = suma +5 ; 
+console.log(suma);
+
+// For 
+for (var i=0 ; i<10 ; i++){
+    suma = suma +1;
+    console.log('variable de iteracion:' , i);
+}
+
+console.log('variable suma es : ', suma);
+
+//WHILE 
+
+while (suma < 3){
+    suma  = suma + 1;
+    console.log(suma);
+}
